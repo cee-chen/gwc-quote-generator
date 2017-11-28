feb023251f28bd22992a0f91c425b26a193db120
Constance <constancecchen@users.noreply.github.com>
2017-11-27 16:27:46 -0800

    Copied files over from Cloud9

---
+++ js/arrays.js
@@ -0,0 +1,32 @@
+window.quotes = {
+    "Star Wars": [
+        "I am your father. <span>- Darth Vader</span>",
+        "From my point of view, the Jedi are evil. <span>- Anakin Skywalker</span>",
+        "I hate sand, it's coarse and it gets everywhere <span>- Anakin Skywalker</span>",
+        "No! Try not. Do or do not. There is no try. <span>- Yoda</span>",
+        'I find your lack of faith disturbing. <span>- Darth Vader</span>',
+        'Never tell me the odds! <span>- Han Solo</span>',
+        'Well, if droids could think, there would be none of us here, would there? <span>- Obi-wan</span>',
+    ],
+    "Guardians of the Galaxy 2": [
+        "I'm Mary Poppins y'all!<span>- Yondu</span>",
+        "he may have been your father, boy, but he wasn't your daddy.<span>- Yondu</span>",
+        "You shouldn't have killed my mom and squished my Walkman.<span>- Peter Quill</span>",
+        "There are two types of beings in the universe, those who dance, and those who do not.<span>- Drax</span>",
+        "You were the one who wanted to win. And *I* just wanted a sister!<span>- Nebula</span>",
+        " It's time for the Ravagers to rise once again to glory with a new captain... Taserface!<span>- Taserface</span>",
+        "Doesn't eternity get boring?<span>- Peter Quill</span>",
+    ],
+    "The Princess Bride": [
+        'As you wish. <span>- Westley</span>' ,
+        '"We\'ll never survive!"<br>"Nonsense. You\'re only saying that because no one ever has." <span>- Westley to Buttercup</span> ',
+        'Good night, Westley. Good work. Sleep well. I\'ll most likely kill you in the morning. ',
+ '
+
+    ],
+    "Happiness": [
+        "We all live with the objective of being happy; our lives are all different and yet the same.",
+        "Joy multiplies when it is shared among friends.",
+        
+    ],
+};
---
+++ js/script.js
@@ -0,0 +1,64 @@
+/*global $*/
+
+window.latestRandomNumber = -1;
+
+$('select').chosen();
+
+var dropdown = $( '#dropdown' );
+var button = $( '#generate-quote' );
+var $quote = $( '.quote' );
+
+button.click(function() {
+    var dropdownText = dropdown.val();
+
+    var selectedItem = dropdown.find( 'option:selected' );
+    var selectedGroup = selectedItem.parent( 'optgroup' );
+    var label = selectedGroup.attr( 'label' );
+
+    // Background image logic
+    if ( label === 'Movie Quotes' ) {
+        $( 'body' ).removeClass().addClass( 'cinema-bg' );
+    } else if ( label === 'Book Quotes' ) {
+        $( 'body' ).removeClass().addClass( 'library-bg' );
+    } else {
+        $( 'body' ).removeClass();
+    }
+
+    // Quote logic
+    var quotesArray = window.quotes[ dropdownText ];
+
+    if ( quotesArray ) {
+        var quoteNumber = getRandomNumber( 0, quotesArray.length );
+        var quote = quotesArray[ quoteNumber ];
+
+        quoteAnimation( quote );
+    } else {
+        // If we can't find any quotes, reset the text
+        quoteAnimation( 'Click for an awesome random quote' );
+    }
+});
+
+function quoteAnimation( quote ) {
+    $quote.fadeOut(function() {
+        $quote.html( quote );
+        $quote.fadeIn();
+    });
+}
+
+function getRandomNumber(min, max) {
+    if ( max === 1 ) {
+        return 0;
+    }
+
+    var min = Math.ceil(min);
+    var max = Math.floor(max);
+    var number = Math.floor(Math.random() * (max - min)) + min;
+
+    if ( number === window.latestRandomNumber ) {
+        number = getRandomNumber( min, max );
+    } else {
+        window.latestRandomNumber = number;
+    }
+
+    return number;
+}
